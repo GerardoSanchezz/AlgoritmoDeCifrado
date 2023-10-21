376622b3e6fa9f22f84f751871bc3007366d1dae
Alan Ruelas <A01641426@tec.mx>
2023-10-21 14:29:10 -0600

    Language changed to english

--- main.cpp
+++ main.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+
 using namespace std;
 
-char** tablaVigenere();
-void deleteTablaVigenere(char**);
+char** vigenereTable();
+void deleteTable(char**, int);
 void exportarTablaVigenere(char**);
-char interseccion(char**, char);
 
 int main(){
-    char** viegnere = tablaVigenere();
-    // Exportar la tabla al archivo de texto
+    char** viegnere = vigenereTable();
+    
     exportarTablaVigenere(viegnere);
-
-    char firstChar = interseccion(viegnere, 'B');
-
-    cout << "Interseccion de B: " << firstChar << endl;
-    // Eliminar la tabla de memoria
-    deleteTablaVigenere(viegnere);
+   
+    deleteTable(viegnere, 26);
 
     return 0;
 }
 
-void deleteTablaVigenere(char** tabla) {
-    for (int i = 0; i < 26; i++) {
-        delete[] tabla[i];
+void deleteTable(char** table, int n) {
+    for (int i = 0; i < n; i++) {
+        delete[] table[i];
     }
-    delete[] tabla;
+    delete[] table;
 }
 
-char** tablaVigenere() {
+char** vigenereTable() {
     char valores[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     char** tabla = new char*[26];
 
@@ -44,31 +40,18 @@ char** tablaVigenere() {
 }
 
 void exportarTablaVigenere(char** tabla) {
-    // Exportar la tabla al archivo de texto
     ofstream archivo("tabla_vigenere.txt");
 
     if (archivo.is_open()) {
         for (int i = 0; i < 26; i++) {
-            for (int j = 0; j < 25; j++) { // 25 en lugar de 26 para omitir la última columna
+            for (int j = 0; j < 25; j++) {
                 archivo << tabla[i][j] << " ";
             }
-            archivo << "\n"; // Nueva línea después de cada fila
+            archivo << "\n";
         }
         archivo.close();
         cout << "Tabla de Vigenère generada y exportada exitosamente a tabla_vigenere.txt" << endl;
     } else {
         cerr << "No se pudo abrir el archivo para escritura." << endl;
     }
-}
-
-char interseccion(char** tabla, char a) {
-    int fila = 0;
-
-    for (int i = 0; i < 26; i++) {
-        if (tabla[i][0] == a) {
-            fila = i;
-            break;
-        }
-    }
-    return tabla[fila][fila];
 }
